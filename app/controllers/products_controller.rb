681c8ee6ab4bd8677d7333ede4c13ad815ebe041
jlmarrone <jlmarrone@gmail.com>
2013-09-26 15:14:12 -0300

    Some tweaks related to best practices

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,7 +32,7 @@ class ProductsController < ApplicationController
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
         format.json { render action: 'show', status: :created, location: @product }
       else
-        format.html { render action: 'new' }
+        format.html { render 'new' }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: 'edit' }
+        format.html { render 'edit' }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ActiveRecord::Base
 	validates :name, uniqueness: true
   validates :avatar, :attachment_presence => true
 
-  has_attached_file :avatar, 
+  has_attached_file :avatar,
   :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
