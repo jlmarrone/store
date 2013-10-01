e32fa863169f91c18df2f6cfa709429312759d4d
Jose Luis Marrone <jlmarrone@gmail.com>
2013-10-01 17:01:00 -0300

    Added some changes

--- app/models/user/validations.rb
+++ app/models/user/validations.rb
@@ -1,5 +1,6 @@
 class User 
 	module Validations
+    extend ActiveSupport::Concern
 		included do
 			validates :username, presence: true, on: :create, :length => { :maximum => 16 }
 
@@ -8,9 +9,8 @@ class User
       validates_format_of       :email,    :with  => Devise.email_regexp, :allow_blank => false, :if => :email_changed?,
                                 :message => "^Please check that your email address is entered correctly."
 
-      validates_presence_of     :password, :on => :create, :message => "^The password can't be blank"
-      validates_confirmation_of :password, :on => :create, :message => "^The passwords didn't match. Please reenter."
-      validates_length_of       :password, :within => Devise.password_length, :allow_blank => true
+      validates :password, presence: true, length: {minimum: 5, maximum: 120}, on: :create
+      validates :password_confirmation, presence: true, length: {minimum: 5, maximum: 120}, on: :create
 		end
  	end
 end
